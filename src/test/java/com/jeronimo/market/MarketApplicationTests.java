718255a6db994f81a70feeade7395462f23512db
Jeronimo Velez Rojas <velezrojasjeronimo@gmail.com>
2023-12-29 15:13:20 -0500

    Iniciando el proyecto

---
+++ src/test/java/com/jeronimo/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeronimo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
