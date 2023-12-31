40537363843fa4a116f86ad3c19b1e37f9fec34b
Jeronimo Velez Rojas <velezrojasjeronimo@gmail.com>
2023-12-31 16:32:17 -0500

    Añadiendo mapper al proyecto

---
+++ src/main/java/com/jeronimo/market/persistence/ProductoRepository.java
@@ -0,0 +1,50 @@
+package com.jeronimo.market.persistence;
+
+import com.jeronimo.market.domain.Product;
+import com.jeronimo.market.domain.repository.ProductRepository;
+import com.jeronimo.market.persistence.crud.ProductoCrudRepository;
+import com.jeronimo.market.persistence.entity.Producto;
+import com.jeronimo.market.persistence.mapper.ProductMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository implements ProductRepository {
+    private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
+
+    @Override
+    public List<Product> getAll() {
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoria(categoryId);
+        return Optional.of(mapper.toProducts(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto= mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
+    }
+}
--- src/main/java/com/jeronimo/market/persistence/entity/Cliente.java
+++ src/main/java/com/jeronimo/market/persistence/entity/Cliente.java
@@ -2,6 +2,8 @@ package com.jeronimo.market.persistence.entity;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "clientes")
 public class Cliente {
